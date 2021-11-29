417f7af6d40b0bd9cb868f2c0960946a339280ac
최민우 <woasidh1@naver.com>
2021-11-30 03:03:16 +0900

    https://programmers.co.kr/learn/courses/30/lessons/42862

--- python/programmers/level1/12937.py
+++
@@ -1,8 +0,0 @@
-def solution(num):
-    answer = 'Even' if num % 2 == 0 else 'Odd';
-    return answer;
-
-input = [3, 4];
-
-for i in input:
-    solution(i);
---
+++ python/programmers/level1/42862.py
@@ -0,0 +1,49 @@
+def solution(n, lost, reserve):
+    answer = 0
+
+    total = [-1];
+
+    for i in range (1, n + 1):
+        total.append(1);
+
+    for i in lost:
+        total[i] -= 1;
+    for i in reserve:
+        total[i] += 1;
+
+    print(total);
+
+    for idx, i in enumerate(total):
+        if i == -1:
+            continue;
+        if i != 2:
+            continue;
+        if idx == 1:
+            checkRight(total, idx);
+        elif idx == n:
+            checkLeft(total, idx);
+        else:
+            if checkLeft(total, idx) != True:
+                checkRight(total, idx);
+
+    for i in total:
+        if i >= 1:
+            answer += 1;
+    return answer;
+
+def checkLeft(arr, idx):
+    if arr[idx - 1] == 0:
+        arr[idx - 1] = 1;
+        arr[idx] = 1;
+        return True;
+    return False;
+
+def checkRight(arr, idx):
+    if arr[idx + 1] == 0:
+        arr[idx + 1] = 1;
+        arr[idx] = 1;
+        return True;
+    return False;
+
+print('hi');
+print(solution(5, [2, 4], [1, 3, 5]));
